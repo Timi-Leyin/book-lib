34105e90fe47e63eeef4cc22ccfb2f374ae266df
timileyin oyelekan <timileyinoyelekan11@gmail.com>
2022-02-05 02:52:30 +0100

    Heroku deploy

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const app =require("./app");
  const mongoose =require("mongoose")
 const config =require("./config/index")
  
- 
  mongoose.connect(config.db.uri, {useNewUrlParser:true, useUnifiedTopology:true  }).then(()=> console.log("Database started "))
  
 .catch(err=> console.log(err))
-app.listen(process.env.PORT, ()=> console.log("Server is running on http://localhost:",process.env.PORT))
+app.listen(process.env.PORT || 5000, ()=> console.log("Server is running on http://localhost:",process.env.PORT))
